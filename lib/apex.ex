50e79c82a0f094e1a9ca67a4ba101623cc398fca
Brandon Gottlob <info@bgottlob.com>
2020-05-13 08:58:24 -0400

    Create packet capture and test server

--- lib/apex.ex
+++ lib/apex.ex
@@ -11,18 +11,27 @@ defmodule Apex do
     listen(socket)
   end
 
-  # For test purposes: read a single UDP packet from a file
-  def start(_type, [pkt_file]) do
-    {:ok, data} = File.read(pkt_file)
-    parse(data)
-    # Return something for mix run to succeed
-    Task.start(fn -> :timer.sleep(1000) end)
+  def start(_type, [capture: file]) do
+    {:ok, socket} = :gen_udp.open(20777, [:binary, active: false])
+    listen(socket, capture: file)
   end
 
-  def listen(socket) do
+  def start(_type, [read: file]) do
+    {:ok, socket} = :gen_udp.open(20777, [:binary, active: false])
+    Task.start(fn -> 
+      Process.sleep(2000)
+      Apex.TestServer.start(nil, [from: file])
+    end)
+    listen(socket, capture: file)
+  end
+
+  def listen(socket, opts \\ []) do
     {:ok, {_srcip, _srcport, data}} = :gen_udp.recv(socket, 0)
+    if opts[:capture] do
+      File.write(opts[:capture], "#{Base.encode16(data)}\n", [:append])
+    end
     parse(data)
-    listen(socket)
+    listen(socket, opts)
   end
 
   def parse(data) do
