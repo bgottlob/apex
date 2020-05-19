36385095448839bf91966e13d7b8e62b649373a7
Brandon Gottlob <info@bgottlob.com>
2020-05-19 08:29:19 -0400

    Hue shift light

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,8 @@
 use Mix.Config
 
 import_config "../apps/*/config/config.exs"
+
+config :apex_shift_bulb,
+  bridge_address: System.get_env("APEX_HUE_BRIDGE_ADDRESS"),
+  username: System.get_env("APEX_HUE_USERNAME"),
+  light_number: System.get_env("APEX_HUE_LIGHT_NUMBER")
