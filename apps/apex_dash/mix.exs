8eeaa5a5913f35a5615b5fff7b14a2d080eebbbf
Brandon Gottlob <info@bgottlob.com>
2020-07-17 20:44:30 -0400

    Upgrade Apex Dash dependencies

--- apps/apex_dash/mix.exs
+++ apps/apex_dash/mix.exs
@@ -36,14 +36,14 @@ defmodule ApexDash.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
-      {:phoenix_pubsub, "~> 2.0.0"},
-      {:phoenix_html, "~> 2.14.2"},
-      {:phoenix_live_reload, "~> 1.2.2", only: :dev},
-      {:gettext, "~> 0.18.0"},
-      {:plug_cowboy, "~> 2.2.1"},
+      {:phoenix, "1.5.3"},
+      {:phoenix_pubsub, "2.0.0"},
+      {:phoenix_html, "2.14.2"},
+      {:phoenix_live_reload, "1.2.4", only: :dev},
+      {:gettext, "0.18.0"},
+      {:plug_cowboy, "2.3.0"},
       {:apex, in_umbrella: true},
-      {:phoenix_live_view, "~> 0.12.1"},
+      {:phoenix_live_view, "0.14.1"},
       {:jason, "~> 1.2.1"},
       {:gen_stage, "~> 1.0.0"}
     ]
