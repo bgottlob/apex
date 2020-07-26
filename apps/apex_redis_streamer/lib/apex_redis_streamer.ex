6588a96c23398943f0d764c3c58387a36f2b2f90
Brandon Gottlob <info@bgottlob.com>
2020-07-26 17:36:47 -0400

    Add apex_redis_streamer application

---
+++ apps/apex_redis_streamer/lib/apex_redis_streamer.ex
@@ -0,0 +1,18 @@
+defmodule ApexRedisStreamer do
+  @moduledoc """
+  Documentation for `ApexRedisStreamer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ApexRedisStreamer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
