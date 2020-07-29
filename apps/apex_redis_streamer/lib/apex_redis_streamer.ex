753def58cc793f2d04b7020f5f08adf53afbc292
Brandon Gottlob <info@bgottlob.com>
2020-07-29 16:45:37 -0400

    Use flow to forward packets to redis

--- apps/apex_redis_streamer/lib/apex_redis_streamer.ex
+++ apps/apex_redis_streamer/lib/apex_redis_streamer.ex
@@ -1,26 +1,9 @@
 defmodule ApexRedisStreamer do
-  use GenStage
+  use Flow
 
-  def start_link(uri, stream) do
-    IO.puts "Connecting to #{uri}"
-    {:ok, stage} = Brink.Producer.start_link(redis_uri: uri, stream: stream, maxlen: 5000)
-    GenStage.start_link(__MODULE__, stage)
-  end
-
-  def init(stage) do
-    case :global.whereis_name(ApexBroadcast) do
-      :undefined ->
-        {:stop, "Unable to find ApexBroadcast producer process"}
-      pid ->
-        {:consumer, stage, subscribe_to: [{pid, [max_demand: 5]}]}
-    end
-  end
-
-  def handle_events(events, _from, stage) do
-    Flow.from_enumerable(events)
+  def start_link(brink_stage) do
+    Flow.from_stages([{:global, ApexBroadcast}])
     |> Flow.map(&(Apex.RedisStreamSerialize.to_redis_stream_map(&1)))
-    |> Flow.into_stages([stage])
-
-    {:noreply, [], stage}
+    |> Flow.into_stages([brink_stage])
   end
 end
--- apps/apex_redis_streamer/lib/application.ex
+++ apps/apex_redis_streamer/lib/application.ex
@@ -9,13 +9,15 @@ defmodule ApexRedisStreamer.Application do
       true ->
         IO.puts "Connected to Apex Broadcast"
         :global.sync()
+        {:ok, brink_stage} = Brink.Producer.start_link(
+          redis_uri: System.get_env("APEX_REDIS_URI"),
+          stream: "telemetry",
+          maxlen: 5000
+        )
+
         children = [%{
           id: ApexRedisStreamer,
-          start: {
-            ApexRedisStreamer,
-            :start_link,
-            [System.get_env("APEX_REDIS_URI"), "telemetry"]
-          },
+          start: {ApexRedisStreamer, :start_link, [brink_stage]},
           restart: :permanent
         }]
         Supervisor.start_link(children, strategy: :one_for_one)
