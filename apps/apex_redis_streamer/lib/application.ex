35d09c8655510a02070c42e73afb87846df42321
Brandon Gottlob <info@bgottlob.com>
2020-08-10 12:24:29 -0400

    Split lap events into per-driver Redis entries

--- apps/apex_redis_streamer/lib/application.ex
+++ apps/apex_redis_streamer/lib/application.ex
@@ -12,14 +12,26 @@ defmodule ApexRedisStreamer.Application do
         {:ok, brink_stage} = Brink.Producer.start_link(
           redis_uri: System.get_env("APEX_REDIS_URI"),
           stream: "telemetry",
-          maxlen: 5000
+          maxlen: 5000,
+          name: Brink.Producer.Telemetry
         )
 
-        children = [%{
-          id: ApexRedisStreamer,
-          start: {ApexRedisStreamer, :start_link, [brink_stage]},
-          restart: :permanent
-        }]
+        {:ok, brink_stage_lap} = Brink.Producer.start_link(
+          redis_uri: System.get_env("APEX_REDIS_URI"),
+          stream: "laps",
+          maxlen: 5000,
+          name: Brink.Producer.Laps
+        )
+
+        children = [
+          %{id: ApexRedisStreamer,
+            start: {ApexRedisStreamer, :start_link, [brink_stage]},
+            restart: :permanent},
+          %{id: ApexRedisStreamer.LapDataStreamer,
+            start: {ApexRedisStreamer.LapDataStreamer,
+                    :start_link,
+                    [brink_stage_lap]}}
+        ]
         Supervisor.start_link(children, strategy: :one_for_one)
       _ ->
         {:error, "Unable to connect to Apex Broadcast, exiting"}
