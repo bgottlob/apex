22da98b6884f0d6dee1ec71ac17ecad3aa522907
Brandon Gottlob <info@bgottlob.com>
2020-08-12 23:12:10 -0400

    Update PaceUpdate struct, add car index to sockets

--- apps/apex/lib/pace_update.ex
+++ apps/apex/lib/pace_update.ex
@@ -1,7 +1,15 @@
 defmodule PaceUpdate do
-  defstruct [:value]
+  @moduledoc """
+  Struct representing a the pace measurement of a given driver during a session
+  """
+  defstruct [:session_uid, :car_index, :lap, :pace]
 
   def from_redis_stream_entry({_id, map}) do
-    struct(__MODULE__, Map.update!(map, :value, &(String.to_float(&1))))
+    # Perform conversions from strings to proper data types
+    struct(__MODULE__, map)
+    |> Map.update!(:session_uid, &(String.to_integer(&1)))
+    |> Map.update!(:car_index, &(String.to_integer(&1)))
+    |> Map.update!(:lap, &(String.to_integer(&1)))
+    |> Map.update!(:pace, &(String.to_float(&1)))
   end
 end
