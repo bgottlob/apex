8af960c2f7f4c4fa13769b5fea8647e1a53a27c8
Brandon Gottlob <info@bgottlob.com>
2020-08-31 22:38:54 -0400

    Add property based tests for data types

--- apps/apex/test/data_types_test.exs
+++ apps/apex/test/data_types_test.exs
@@ -1,5 +1,6 @@
 defmodule DataTypesTest do
   use ExUnit.Case
+  use PropCheck
 
   # https://www.h-schmidt.net/FloatConverter/IEEE754.html
   test "static examples of float32" do
@@ -88,5 +89,115 @@ defmodule DataTypesTest do
     <<a,b>> = <<0xFFFF::16>>
     {result, _rest} = F1.DataTypes.int16({%{}, <<b,a>>}, :result)
     assert result[:result] == -1
+  end
+
+  def valid_little_float32?(b) do
+    <<a,b,c,d>> = b
+    <<_sign::integer-size(1), exp::integer-size(8), _mant::integer-size(23)>> = <<d,c,b,a>>
+    exp < 0xFF
+  end
+
+  # Generator
+  def float32_bitstring do
+    such_that b <- bitstring(32), when: valid_little_float32?(b)
+  end
+
+  property "the correct number of bytes are processed for float32" do
+    forall b <- float32_bitstring() do
+      {result, rest} = F1.DataTypes.float32({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for uint8" do
+    forall b <- bitstring(8) do
+      {result, rest} = F1.DataTypes.uint8({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for uint16" do
+    forall b <- bitstring(16) do
+      {result, rest} = F1.DataTypes.uint16({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for uint32" do
+    forall b <- bitstring(32) do
+      {result, rest} = F1.DataTypes.uint32({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for uint64" do
+    forall b <- bitstring(64) do
+      {result, rest} = F1.DataTypes.uint64({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for int8" do
+    forall b <- bitstring(8) do
+      {result, rest} = F1.DataTypes.int8({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "the correct number of bytes are processed for int16" do
+    forall b <- bitstring(16) do
+      {result, rest} = F1.DataTypes.int16({%{}, b}, :result)
+      Map.has_key?(result, :result) && rest == <<>>
+    end
+  end
+
+  property "parsed float32 values are within acceptable range" do
+    forall b <- float32_bitstring() do
+      {r, _rest} = F1.DataTypes.float32({%{}, b}, :result)
+      r[:result] >= -340282346638528859811704183484516925440 &&
+        r[:result] <= 340282346638528859811704183484516925440
+    end
+  end
+
+  property "parsed uint8 values are within acceptable range" do
+    forall b <- bitstring(8) do
+      {r, _rest} = F1.DataTypes.uint8({%{}, b}, :result)
+      r[:result] >= 0 && r[:result] <= 255
+    end
+  end
+
+  property "parsed uint16 values are within acceptable range" do
+    forall b <- bitstring(16) do
+      {r, _rest} = F1.DataTypes.uint16({%{}, b}, :result)
+      r[:result] >= 0 && r[:result] <= 65535
+    end
+  end
+
+  property "parsed uint32 values are within acceptable range" do
+    forall b <- bitstring(32) do
+      {r, _rest} = F1.DataTypes.uint32({%{}, b}, :result)
+      r[:result] >= 0 && r[:result] <= 4294967295
+    end
+  end
+
+  property "parsed uint64 values are within acceptable range" do
+    forall b <- bitstring(64) do
+      {r, _rest} = F1.DataTypes.uint64({%{}, b}, :result)
+      r[:result] >= 0 && r[:result] <= 18446744073709551615
+    end
+  end
+
+  property "parsed int8 values are within acceptable range" do
+    forall b <- bitstring(8) do
+      {r, _rest} = F1.DataTypes.int8({%{}, b}, :result)
+      r[:result] >= -128 && r[:result] <= 127
+    end
+  end
+
+  property "parsed int16 values are within acceptable range" do
+    forall b <- bitstring(16) do
+      {r, _rest} = F1.DataTypes.int16({%{}, b}, :result)
+      r[:result] >= -32768 && r[:result] <= 32767
+    end
   end
 end
